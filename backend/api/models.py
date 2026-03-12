4b4bb5447f43482e7c343650af56cdff63794455
Huzaifa Rashid <huzaifaras10@gmail.com>
2026-03-12 05:04:11 -0700

    update backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -42,6 +42,7 @@ class Vendor(models.Model):
 class Item(models.Model):
     name = models.CharField(max_length=200, unique=True)
     unit = models.CharField(max_length=50) # e.g., KG, Litre
+    image = models.ImageField(upload_to='item_images/', blank=True, null=True)
     
     def __str__(self):
         return f"{self.name} ({self.unit})"
