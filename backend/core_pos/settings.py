b770ee084b46c20da45089acc66e929d09fd1e3f
Huzaifa Rashid <huzaifaras10@gmail.com>
2026-03-12 09:57:54 -0700

    .

--- backend/core_pos/settings.py
+++ backend/core_pos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,4 +140,8 @@ REST_FRAMEWORK = {
     )
 }
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),  # Token lasts for 1 day instead of 5 minutes
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
 
